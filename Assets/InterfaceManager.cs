8c015150cf999a03354bfaee8e23450107d35cc3
Xazaz <Joshuateo.29@gmail.com>
2017-11-07 23:08:38 +0800

    Basic Menu

---
+++ Assets/InterfaceManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterfaceManager : MonoBehaviour {
+    public GameObject optionsMenu;
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+    public void clickStart() {
+        Application.LoadLevel(1);
+}
+    public void clickOption()
+    {
+        
+        
+        if(optionsMenu.activeInHierarchy == true)
+        {
+            optionsMenu.SetActive(false);
+            Debug.Log(false);
+        }
+        else
+        {
+            optionsMenu.SetActive(true);
+            Debug.Log(true);
+        }
+    }
+}
